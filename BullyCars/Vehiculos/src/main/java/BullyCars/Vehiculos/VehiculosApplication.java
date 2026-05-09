c567f72d41ec9ad2da91a6d51d4cc25b4e35900e
Kaemeele <cami.perezs@duocuc.cl>
2026-05-08 20:48:56 -0400

    Add files via upload

---
+++ BullyCars/Vehiculos/src/main/java/BullyCars/Vehiculos/VehiculosApplication.java
@@ -0,0 +1,13 @@
+package BullyCars.Vehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculosApplication.class, args);
+	}
+
+}
